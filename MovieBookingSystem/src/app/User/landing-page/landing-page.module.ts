ddb8293575522c348c34d8fe9892548862892e70
Anvee <anveesah@gmail.com>
2022-06-13 12:21:19 +0530

    initial commit

---
+++ MovieBookingSystem/src/app/User/landing-page/landing-page.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LandingPageRoutingModule } from './landing-page-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LandingPageRoutingModule
+  ]
+})
+export class LandingPageModule { }
