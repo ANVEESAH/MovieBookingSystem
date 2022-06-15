72e77da7fe13fb913addaa2c219c2cf8a260e523
Your Name <sagarbhadauria094@gmail.com>
2022-06-15 13:50:38 +0530

    routing done

--- MovieBookingSystem/src/app/user-layout/user-layout-routing.module.ts
+++ MovieBookingSystem/src/app/user-layout/user-layout-routing.module.ts
@@ -1,15 +1,20 @@
 import {  Routes } from '@angular/router';
+import { BookTicketComponent } from '../user/book-ticket/book-ticket.component';
 import { ContentComponent } from '../user/content/content.component';
 import { ErrorComponent } from '../user/error/error.component';
+import { HomeComponent } from '../user/home/home.component';
+
 import { HeaderComponent } from './header/header.component';
 
 const routes: Routes = [
   {path:'header',component: HeaderComponent},
-  // {path:'',component: ErrorComponent},
+  {path:'',component: HomeComponent},
+  {path:'bookticket',component: BookTicketComponent}
+ 
+  {path:'home',component: HomeComponent},
   {path:'content',component: ContentComponent},
   {path:'error',component: ErrorComponent},
-  {path:'**',component: ErrorComponent
-  }
+  
 ];
 
 
